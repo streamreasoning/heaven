b455c854dc01baa4772f451b763ed0b6bc2460cc
riccardotommasini <tomma156@gmail.com>
2014-12-12 11:12:20 +0100

    exploit internal event saving

--- src/main/java/it/polimi/processing/collector/ResultCollector.java
+++ src/main/java/it/polimi/processing/collector/ResultCollector.java
@@ -2,10 +2,8 @@ package it.polimi.processing.collector;
 
 import it.polimi.processing.workbench.core.EventProcessor;
 
-import java.io.IOException;
-
 public interface ResultCollector<Event> extends EventProcessor<Event> {
 
-	public boolean process(Event r, String where) throws IOException;
+	public boolean process(Event r, String where);
 
 }
