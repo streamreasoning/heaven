af4d574fb2f95d93668a0371bb2113f03f5051de
riccardotommasini <tomma156@gmail.com>
2014-12-13 09:29:49 +0100

    moving services in processing package

---
+++ src/main/java/it/polimi/processing/services/FileService.java
@@ -0,0 +1,56 @@
+package it.polimi.processing.services;
+
+import it.polimi.processing.collector.saver.data.CollectableData;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class FileService {
+
+	public static boolean write(String where, CollectableData d) {
+		try {
+			FileWriter writer;
+			File file = new File(where);
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+			writer = new FileWriter(file, true);
+			writer.write(d.getData());
+			writer.flush();
+			writer.close();
+			return true;
+		} catch (IOException e) {
+			log.info(e.getMessage());
+			return false;
+		}
+	}
+
+	public static void createFolders(String vsfolder) {
+		new File(vsfolder).mkdirs();
+	}
+
+	public static void createFolder(String vsfolder) {
+		new File(vsfolder).mkdir();
+	}
+
+	public static void createOutputFolder(String folder) {
+		new File(folder).mkdirs();
+	}
+
+	public static BufferedReader getBuffer(String fileName) {
+		try {
+			File file = new File(fileName);
+			return new BufferedReader(new FileReader(file));
+		} catch (FileNotFoundException e) {
+			log.error(e.getMessage());
+			return null;
+		}
+	}
+}
--- src/main/java/it/polimi/processing/services/SQLListeService.java
+++ src/main/java/it/polimi/processing/services/SQLListeService.java
@@ -1,7 +1,6 @@
-package it.polimi.processing.collector;
+package it.polimi.processing.services;
 
 import it.polimi.processing.collector.saver.data.SQL;
-import it.polimi.utils.DatabaseUtils;
 import it.polimi.utils.FileUtils;
 
 import java.sql.Connection;
@@ -21,14 +20,25 @@ public class SQLListeService {
 	private static Connection c;
 	private static Statement stmt = null;
 
+	public static final String EXPERIMENT_TABLE = "CREATE TABLE IF NOT EXISTS EXPERIMENT " + "(" + " EXP_ID       TEXT                 NOT NULL, "
+			+ "	TS_INIT      TEXT                 NOT NULL, " + "	TS_END       TEXT                 NOT NULL, "
+			+ " ENGINE       TEXT   			  NOT NULL, " + " INPUT_FILE   TEXT   			  NOT NULL, " + " RESULT_FILE  TEXT                 NOT NULL, "
+			+ " LOG_FILE     TEXT                 NOT NULL, " + " PRIMARY      KEY( EXP_ID,TS_INIT )        ) ";
+	public static final String EXPERIMENT_INSERT = "INSERT INTO EXPERIMENT ( EXP_ID, TS_INIT, TS_END, ENGINE, INPUT_FILE,RESULT_FILE,LOG_FILE ) ";
+	public static final String COMPARATION_TABLE = "CREATE TABLE IF NOT EXISTS COMPARATION " + "(" + " EXP_ID       TEXT                 NOT NULL, "
+			+ "	EVENT_ID     TEXT                 NOT NULL, " + "	SOUND        INTEGER              NOT NULL, "
+			+ " COMPLETE     INTEGER   	          NOT NULL, " + " MEMORY       REAL                 NOT NULL, "
+			+ " LATENCY      INTEGER              NOT NULL, " + " PRIMARY      KEY( EXP_ID,EVENT_ID )       ) ";
+	public static final String COMPARATION_INSERT = "INSERT INTO COMPARATION (EXP_ID, EVENT_ID, SOUND, COMPLETE,MEMORY, LATENCY) ";
+
 	static {
 		try {
 			Class.forName(ORG_SQLITE_JDBC);
 			c = DriverManager.getConnection(JDBC_SQLITE_OBQAATCEP_DB);
 			log.info("Opened database successfully");
 			stmt = c.createStatement();
-			stmt.executeUpdate(DatabaseUtils.EXPERIMENT_TABLE);
-			stmt.executeUpdate(DatabaseUtils.COMPARATION_TABLE);
+			stmt.executeUpdate(EXPERIMENT_TABLE);
+			stmt.executeUpdate(COMPARATION_TABLE);
 			stmt.close();
 			log.info("Table created successfully");
 		} catch (ClassNotFoundException e) {
@@ -44,7 +54,7 @@ public class SQLListeService {
 			String sql = d.getData();
 			log.info(sql);
 			if (sql != null && stmt != null && c != null && !c.isClosed()) {
-				stmt.executeUpdate(DatabaseUtils.EXPERIMENT_INSERT + sql);
+				stmt.executeUpdate(EXPERIMENT_INSERT + sql);
 				stmt.close();
 				return true;
 			}
@@ -63,4 +73,5 @@ public class SQLListeService {
 			return false;
 		}
 	}
+
 }
