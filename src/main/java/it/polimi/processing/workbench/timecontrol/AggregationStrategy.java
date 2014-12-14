8d78553af805cbf383bdd9b0b16caa9a005fe20e
riccardotommasini <tomma156@gmail.com>
2014-12-15 00:36:36 +0100

    bugfix on aggregation, now it counts event befor sending time in advace in order to aggregate incoming events

--- src/main/java/it/polimi/processing/workbench/timecontrol/AggregationStrategy.java
+++ src/main/java/it/polimi/processing/workbench/timecontrol/AggregationStrategy.java
@@ -3,6 +3,7 @@ package it.polimi.processing.workbench.timecontrol;
 import it.polimi.processing.events.RSPEvent;
 import it.polimi.processing.events.TSResult;
 import it.polimi.processing.rspengine.abstracts.RSPEngine;
+import it.polimi.processing.rspengine.shared.events.EsperUtils;
 import it.polimi.processing.system.GetPropertyValues;
 import it.polimi.processing.system.Memory;
 import lombok.extern.log4j.Log4j;
@@ -13,7 +14,7 @@ public class AggregationStrategy implements TimeStrategy {
 	private int tsEventNumber;
 	private final int aggregation;
 	private RSPEngine rspEngine;
-	private TSResult currentResult;
+	private final TSResult currentResult;
 	private final int experiment;
 	private final int tsEventMax;
 
@@ -22,6 +23,7 @@ public class AggregationStrategy implements TimeStrategy {
 		this.experiment = GetPropertyValues.getIntegerProperty("experiment_number");
 		this.tsEventNumber = 0;
 		this.tsEventMax = aggregation - 1;
+		currentResult = new TSResult();
 	}
 
 	@Override
@@ -31,21 +33,19 @@ public class AggregationStrategy implements TimeStrategy {
 		int eventNumber = rspEngine.getEventNumber();
 
 		if (tsEventNumber == 0) {
-			currentResult = new TSResult(System.currentTimeMillis(), Memory.getMemoryUsage());
+			currentResult.setInputTimestamp(System.currentTimeMillis());
+			currentResult.setMemoryB(Memory.getMemoryUsage());
+			currentResult.setEventNumber(eventNumber / aggregation);
+			currentResult.setId("<http://example.org/" + experiment + "/" + eventNumber / aggregation + ">");
 			tsEventNumber++;
 		} else if (tsEventNumber == tsEventMax) {
 			tsEventNumber = 0;
+			rspEngine.progress(EsperUtils.WINDOW_SIZE / EsperUtils.OUTPUT_RATE);
 		} else {
-			currentResult.setInputTimestamp(System.currentTimeMillis());
-			currentResult.setMemoryB(Memory.getMemoryUsage());
 			tsEventNumber++;
 		}
 
-		currentResult.setEventNumber(eventNumber);
-		currentResult.setId("<http://example.org/" + experiment + "/" + eventNumber + ">");
-		process = rspEngine.process(e);
-		rspEngine.progress(aggregation);
-		return process;
+		return rspEngine.process(e);
 	}
 
 	@Override
