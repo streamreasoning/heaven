7fc2ecaa7e0fc86c193bc0f58233dd39ea0d23b2
riccardotommasini <tomma156@gmail.com>
2014-12-15 00:27:54 +0100

    calculate init size basing on aggregationa and triple in window

--- src/main/java/it/polimi/preprocessor/ExperimentGenerator.java
+++ src/main/java/it/polimi/preprocessor/ExperimentGenerator.java
@@ -1,4 +1,4 @@
-package it.polimi.postprocessing;
+package it.polimi.preprocessor;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -14,11 +14,12 @@ import lombok.extern.log4j.Log4j;
 public class ExperimentGenerator {
 
 	private static Properties sourceProp;
+	private static boolean autoCalcInitSize = true;
 
 	public static void main(String[] args) throws IOException {
 		String pathToClone = "properties/";
 		String inputName = "default_teststand.properties";
-		String outputName = "smpl_tevent_latency_1000_10_EN";
+		String outputName = "prova_auto";
 		sourceProp = new Properties();
 		InputStream inputStream = ExperimentGenerator.class.getClassLoader().getResourceAsStream(pathToClone + inputName);
 		if (inputStream != null)
@@ -35,10 +36,24 @@ public class ExperimentGenerator {
 				dest_prop.put(key, sourceProp.get(key));
 			}
 
-			dest_prop.setProperty("execution_number", i + "");
+			if (autoCalcInitSize)
+				calculateInitSize(i, dest_prop);
+
 			OutputStream out = new FileOutputStream(f);
 			dest_prop.store(out, "This is an optional header comment string");
 		}
 
+	}
+
+	protected static void calculateInitSize(int i, Properties dest_prop) {
+		final int eventInWindow = 10;
+		dest_prop.setProperty("execution_number", i + "");
+		if (Boolean.parseBoolean(dest_prop.getProperty("external_time_control_on")) && "AGGREGATION".equals(dest_prop.getProperty("time_strategy"))) {
+			int triple_in_window = new Integer(dest_prop.getProperty("triple_in_window"));
+			int aggregation = new Integer(dest_prop.getProperty("aggregation"));
+
+			dest_prop.setProperty("init_size", (triple_in_window / (eventInWindow * aggregation) + ""));
+			dest_prop.setProperty("rspevent_number", (triple_in_window * aggregation) + "");
+		}
 	}
 }
