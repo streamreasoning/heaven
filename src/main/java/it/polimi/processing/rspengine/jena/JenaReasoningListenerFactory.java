7a6998d31fb0ed08a1bd5c80b69ae72b4b4b9082
riccardotommasini <tomma156@gmail.com>
2015-01-07 23:35:52 +0100

    better log

--- src/main/java/it/polimi/processing/rspengine/jena/JenaReasoningListenerFactory.java
+++ src/main/java/it/polimi/processing/rspengine/jena/JenaReasoningListenerFactory.java
@@ -11,34 +11,42 @@ import it.polimi.processing.rspengine.jena.timekeeping.external.snapshot.listene
 import it.polimi.processing.rspengine.jena.timekeeping.external.snapshot.listener.JenaSMPLListener;
 import it.polimi.utils.FileUtils;
 import it.polimi.utils.RDFSUtils;
+import lombok.extern.log4j.Log4j;
 
 import com.espertech.esper.client.UpdateListener;
 
+@Log4j
 public final class JenaReasoningListenerFactory {
 
 	private static UpdateListener listener;
 
 	public static UpdateListener getSMPLListener(EventProcessor<Event> next) {
+		log.info("Select SMPLListener ");
 		return listener = new JenaSMPLListener(RDFSUtils.loadModel(FileUtils.UNIV_BENCH_RDFS_MODIFIED), next);
 	}
 
 	public static UpdateListener getRhoDfListener(EventProcessor<Event> next) {
+		log.info("Select RHODFListener ");
 		return listener = new JenaRhoDFListener(RDFSUtils.loadModel(FileUtils.UNIV_BENCH_RHODF_MODIFIED), FileUtils.RHODF_RULE_SET_RUNTIME, next);
 	}
 
 	public static UpdateListener getFULLListener(TestStand next) {
+		log.info("Select FullListener ");
 		return listener = new JenaFullListener(RDFSUtils.loadModel(FileUtils.UNIV_BENCH_RHODF_MODIFIED), next);
 	}
 
 	public static UpdateListener getIncrementalSMPLListener(EventProcessor<Event> next) {
+		log.info("Select SMPLListener INC");
 		return listener = new JenaIncSMPLListener(RDFSUtils.loadModel(FileUtils.UNIV_BENCH_RDFS_MODIFIED), next);
 	}
 
 	public static UpdateListener getIncrementalRhoDfListener(EventProcessor<Event> next) {
+		log.info("Select RHODFListener INC  ");
 		return listener = new JenaIncRhoDFListener(RDFSUtils.loadModel(FileUtils.UNIV_BENCH_RHODF_MODIFIED), FileUtils.RHODF_RULE_SET_RUNTIME, next);
 	}
 
 	public static UpdateListener getIncrementalFULLListener(TestStand next) {
+		log.info("Select FullListener INC ");
 		return listener = new JenaIncFullListener(RDFSUtils.loadModel(FileUtils.UNIV_BENCH_RHODF_MODIFIED), next);
 	}
 
