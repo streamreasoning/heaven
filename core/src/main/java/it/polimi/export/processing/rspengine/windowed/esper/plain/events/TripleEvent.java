90aa2f47cf0654b0fb6d93fcf62fa6685a03c067
riccardotommasini <tomma156@gmail.com>
2015-08-29 13:00:05 -0400

    post processing separated from core and exported work about e2sr

--- core/src/main/java/it/polimi/export/processing/rspengine/windowed/esper/plain/events/TripleEvent.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.export.processing.rspengine.windowed.esper.plain.events;
-
-import it.polimi.processing.events.TripleContainer;
-
-import java.util.Set;
-
-public interface TripleEvent {
-	public Set<TripleContainer> getTriples();
-}
