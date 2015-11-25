5ece9d3bbcf59d55fb9654de058edba95ae0a6f0
riccardotommasini <tomma156@gmail.com>
2015-11-25 14:24:55 -0500

    added timestamp in constructor

--- core/src/main/java/it/polimi/heaven/core/ts/events/Stimulus.java
+++ core/src/main/java/it/polimi/heaven/core/ts/events/Stimulus.java
@@ -16,26 +16,29 @@ public class Stimulus implements Event {
 	private String id;
 	private Set<TripleContainer> eventTriples;
 	private int eventNumber, experimentNumber;
-	private long inputTimestamp;
+	private long inputTimestamp, timestamp;
 
 	public int size() {
 		return eventTriples.size();
 	}
 
-	public Stimulus(String id, Set<TripleContainer> hashSet, int eventNumber, int experimentNumber) {
+	public Stimulus(String id, Set<TripleContainer> hashSet, int eventNumber, int experimentNumber, long timestamp) {
 		this.id = id;
 		this.eventTriples = hashSet;
 		this.eventNumber = eventNumber;
 		this.experimentNumber = experimentNumber;
 		this.inputTimestamp = System.currentTimeMillis();
+		this.timestamp = timestamp;
 	}
 
-	public Stimulus rebuild(String id, Set<TripleContainer> hashSet, int eventNumber, int experimentNumber) {
+	public Stimulus rebuild(String id, Set<TripleContainer> hashSet, int eventNumber, int experimentNumber, long timestamp) {
+		// TODO edit to include all the parameters
 		this.id = id;
 		this.eventTriples = hashSet;
 		this.eventNumber = eventNumber;
 		this.experimentNumber = experimentNumber;
 		this.inputTimestamp = System.currentTimeMillis();
+		this.timestamp = timestamp;
 		return this;
 	}
 
